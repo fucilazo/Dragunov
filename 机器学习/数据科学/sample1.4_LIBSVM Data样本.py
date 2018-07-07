4bd07c2fd0d91f1e7679b903390b118cdbae9475
Fucilazo <460661362@qq.com>
2018-07-07 12:37:27 +0800

    更新

--- 机器学习/数据科学/sample1.4_LIBSVM Data样本.py
+++ 机器学习/数据科学/sample1.4_LIBSVM Data样本.py
@@ -5,3 +5,4 @@ target_page = 'https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a1
 a2a = urllib.request.urlopen(target_page)
 X_train, Y_train = load_svmlight_file(a2a)
 print(X_train.shape, Y_train.shape)
+
